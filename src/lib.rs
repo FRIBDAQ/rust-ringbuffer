56ebe3207149e8e81a7e9dc57004ab09c15158ce
rfoxmich <fox@nscl.msu.edu>
2022-08-10 08:47:10 -0400

    *   Tested set_producer/free_producer methods. *   Wrote set_consumer method *   Wrote tests for set_consumer. *   Tests for set_consumer pass.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+// *** IMPORTANT - see note prior to tests about how tests must be run.
+
 use memmap::MmapMut;
 use std::fs::OpenOptions;
 use std::mem;
@@ -5,6 +7,7 @@ use std::str;
 use std::string::ToString;
 
 static MAGIC_STRING: &str = "NSCLRing";
+static UNUSED_ENTRY: u32 = 0xffffffff;
 ///
 /// RingHeader
 ///    is the header of a ring buffer:
@@ -26,7 +29,7 @@ pub struct RingHeader {
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct ClientInformation {
     offset: usize, // Put/Get offset.
-    pid: u32,      // Process ID owning or 0xffffffff if free.
+    pid: u32,      // Process ID owning or UNUSED_ENTRY if free.
 }
 
 /// The actual ring buffer is quite a bit simpler in practice:
@@ -52,6 +55,9 @@ impl RingBufferMap {
     fn as_mut_ref(&mut self) -> &mut RingBuffer {
         let p = self.map.as_mut_ptr() as *mut RingBuffer;
         unsafe { &mut *p }
+    }
+    fn valid_consumer(&self, n: usize) -> bool {
+        n < self.max_consumers()
     }
 
     fn check_magic(map: &memmap::MmapMut) -> bool {
@@ -131,18 +137,65 @@ impl RingBufferMap {
     // Controlled mutators:
 
     pub fn set_producer(&mut self, pid: u32) -> Result<u32, String> {
-        Err(String::from("unimplemented"))
-    }
-    pub fn set_consumer(&mut self, pid: u32, n: isize) -> Result<u32, String> {
-        Err(String::from("Unimplemented"))
+        let mut producer = self.producer();
+        if (producer.pid == UNUSED_ENTRY) || (producer.pid == pid) {
+            producer.pid = pid;
+            Ok(pid)
+        } else {
+            Err(format!(
+                "Producer is already allocated to PID {}",
+                producer.pid
+            ))
+        }
     }
     pub fn free_producer(&mut self, pid: u32) -> Result<u32, String> {
-        Err(String::from("Unimplemented"))
+        let mut producer = self.producer();
+
+        // We can only free entries which are already free or
+        // that the pid owns:
+
+        if (producer.pid == UNUSED_ENTRY) || (producer.pid == pid) {
+            producer.pid = UNUSED_ENTRY;
+            Ok(pid)
+        } else {
+            Err(format!(
+                "PID {} is unable to free producer owned by {}",
+                pid, producer.pid
+            ))
+        }
     }
+    pub fn set_consumer(&mut self, n: usize, pid: u32) -> Result<u32, String> {
+        let producer_offset = self.producer().offset; // snapshot producer offset.
+        match self.consumer(n) {
+            Ok(cons) => {
+                // Note that self ownership fails since we modify the
+                // offset.
+                if cons.pid == UNUSED_ENTRY {
+                    cons.pid = pid;
+                    cons.offset = producer_offset; //Skip to producer pos.
+                    Ok(pid)
+                } else {
+                    Err(format!(
+                        "Consumer {} is already allocated to {}",
+                        n, cons.pid
+                    ))
+                }
+            }
+            Err(reason) => Err(reason),
+        }
+    }
+
     pub fn free_consumer(&mut self, n: isize, pid: u32) -> Result<u32, String> {
         Err(String::from("Unimplemented"))
     }
 }
+
+// Note the tests below must be run:
+//  cargo test --lib -- --test-threads=1
+// This is because all tests use the same ringbuffer and
+// there are therefore race conditions that can occur between threads
+// as ring buffer access is _not_ threadsafe.   If threaded use
+// of a ring buffer is desired it should be wrapped in a mutex  e.g.
 
 #[cfg(test)]
 mod tests {
@@ -167,13 +220,7 @@ mod tests {
     fn get_producer() {
         let mut ring = RingBufferMap::new("poop").unwrap();
         let p = ring.producer();
-        assert_eq!(
-            ClientInformation {
-                offset: 1355626,
-                pid: 0xffffffff,
-            },
-            *p
-        );
+        assert_eq!(UNUSED_ENTRY, p.pid);
     }
     #[test]
     fn get_consumer_fail() {
@@ -194,9 +241,102 @@ mod tests {
         assert_eq!(
             ClientInformation {
                 offset: offset,
-                pid: 0xffffffff
+                pid: UNUSED_ENTRY
             },
             *consumer
         );
+    }
+    #[test]
+    fn alloc_producer_ok1() {
+        // Client entry is unsed:
+
+        let mut ring = RingBufferMap::new("poop").unwrap();
+        ring.producer().pid = UNUSED_ENTRY;
+        let result = ring.set_producer(1234);
+        assert!(result.is_ok());
+        assert_eq!(1234, result.unwrap());
+
+        ring.producer().pid = UNUSED_ENTRY; // wish I had teardown.
+    }
+    #[test]
+    fn alloc_producer_ok2() {
+        // client entry is used but by us already
+
+        let mut ring = RingBufferMap::new("poop").unwrap();
+
+        ring.producer().pid = 1234;
+        let result = ring.set_producer(1234);
+        assert!(result.is_ok());
+        assert_eq!(1234, result.unwrap());
+        ring.producer().pid = UNUSED_ENTRY; // wish I had teardown.
+    }
+    #[test]
+    fn alloc_producer_fail() {
+        // already owned by someone else:
+
+        let mut ring = RingBufferMap::new("poop").unwrap();
+        ring.producer().pid = 1235;
+        let result = ring.set_producer(1234);
+        assert!(result.is_err());
+        ring.producer().pid = UNUSED_ENTRY;
+    }
+    #[test]
+    fn free_producer_fail() {
+        // Owned all right but by someone else:
+
+        let mut ring = RingBufferMap::new("poop").unwrap();
+        ring.producer().pid = 12345;
+        assert!(ring.free_producer(1234).is_err());
+        ring.producer().pid = UNUSED_ENTRY;
+    }
+    #[test]
+    fn free_producer_ok1() {
+        // if not owned it can be freed by anyone:
+
+        let mut ring = RingBufferMap::new("poop").unwrap();
+        ring.producer().pid = UNUSED_ENTRY;
+        assert!(ring.free_producer(12345).is_ok());
+        assert_eq!(UNUSED_ENTRY, ring.producer().pid);
+    }
+    #[test]
+    fn free_producer_ok2() {
+        // We own the producer so it's ok to release it:
+
+        let mut ring = RingBufferMap::new("poop").unwrap();
+        ring.producer().pid = 1234;
+        assert!(ring.free_producer(1234).is_ok());
+        assert_eq!(UNUSED_ENTRY, ring.producer().pid);
+    }
+    #[test]
+    fn alloc_consumer_fail1() {
+        // Invalid consumer index:
+
+        let mut ring = RingBufferMap::new("poop").unwrap();
+        assert!(ring.set_consumer(ring.max_consumers(), 1).is_err());
+    }
+    #[test]
+    fn alloc_consumer_fail2() {
+        // Already allocated to someone else.
+
+        let mut ring = RingBufferMap::new("poop").unwrap();
+        ring.consumer(1).unwrap().pid = 123;
+        assert!(ring.set_consumer(1, 12345).is_err());
+        ring.consumer(1).unwrap().pid = UNUSED_ENTRY;
+    }
+    #[test]
+    fn alloc_consumer_ok() {
+        let mut ring = RingBufferMap::new("poop").unwrap();
+        ring.consumer(1).unwrap().pid = UNUSED_ENTRY;
+
+        assert!(ring.set_consumer(1, 12345).is_ok());
+
+        assert_eq!(
+            ClientInformation {
+                pid: 12345,
+                offset: ring.producer().offset,
+            },
+            *ring.consumer(1).unwrap()
+        );
+        ring.consumer(1).unwrap().pid = UNUSED_ENTRY;
     }
 }
